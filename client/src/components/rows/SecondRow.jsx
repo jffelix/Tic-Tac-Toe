3ea56e6e100b5142b800e33fc8a3bfff7b598623
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-01-05 02:01:48 -0800

    Added CSS styling to boxes

--- client/src/components/rows/SecondRow.jsx
+++ client/src/components/rows/SecondRow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../../dist/SecondRow.css";
 
 const SecondRow = (props) => {
 
--- client/src/components/rows/ThirdRow.jsx
+++ client/src/components/rows/ThirdRow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../../dist/ThirdRow.css";
 
 const ThirdRow = (props) => {
 
